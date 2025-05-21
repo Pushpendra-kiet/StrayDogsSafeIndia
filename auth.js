1539cab5a3ab1c712bd45309eb22178fd5113c60
programminghunk <pshpndr.kumar@gmail.com>
2025-05-21 13:12:51 +0530

    accept
    all

--- auth.js
+++ auth.js
@@ -1,39 +1,25 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('./models/User');
+
+const USER = {}; // You can later save users in MongoDB
 
 passport.use(new GoogleStrategy({
   clientID: '1067075470360-ujqa07afqodhi7sdbnv5he6303vjr4g2.apps.googleusercontent.com',
   clientSecret: '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',
-  callbackURL: 'https://stray-dogs-safe-india.vercel.app/auth/google/callback'
+  callbackURL: 'https://stray-dogs-safe-india.vercel.app/auth/google/callback',
 },
-async (accessToken, refreshToken, profile, done) => {
-  try {
-    let user = await User.findOne({ googleId: profile.id });
-
-    if (!user) {
-      user = await User.create({
-        googleId: profile.id,
-        name: profile.displayName,
-        email: profile.emails[0].value
-      });
-    }
-
-    return done(null, user);
-  } catch (err) {
-    return done(err, null);
-  }
+function(accessToken, refreshToken, profile, done) {
+  // Simulate storing user in DB
+  USER.googleId = profile.id;
+  USER.name = profile.displayName;
+  USER.email = profile.emails[0].value;
+  return done(null, USER);
 }));
 
 passport.serializeUser((user, done) => {
-  done(null, user._id);
+  done(null, user.googleId);
 });
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch (err) {
-    done(err, null);
-  }
+passport.deserializeUser((id, done) => {
+  done(null, USER); // Should fetch user from DB by id
 });
